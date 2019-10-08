92853f6b7450bf8f757e08b41739e954c7888efa
macro <ZHANGQUAN526@pingan.com.cn>
2019-10-08 16:54:52 +0800

    update

--- project.js
+++ project.js
@@ -14,6 +14,18 @@ module.exports = function(app) {
         });
      })
 
+     app.get('/project/detail/1', function (req, res) {
+        fs.readFile( path + "detail.json", 'utf8', function (err, data) {
+            // console.log( data );
+            // 发送 HTTP 头部
+            // HTTP 状态值: 200 : OK
+            // 内容类型: application/json
+            res.writeHead(200, {'Content-Type': 'application/json;charset=UTF-8',
+                                'Accept': '*/*'});
+            res.end( data );
+        });
+     })
+
      app.get('/group/list', function (req, res) {
         fs.readFile( path + "group_list.json", 'utf8', function (err, data) {
             // console.log( data );

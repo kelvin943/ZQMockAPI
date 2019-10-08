30d93689eb7648e74cb7c615f020a3196e4ebc06
macro <ZHANGQUAN526@pingan.com.cn>
2019-10-08 16:42:44 +0800

    update

--- project.js
+++ project.js
@@ -3,7 +3,7 @@ var path = __dirname + "/" + "resource/local_data/"
 
 module.exports = function(app) {
     app.get('/project/list', function (req, res) {
-        fs.readFile( path + "project_mock_data.json", 'utf8', function (err, data) {
+        fs.readFile( path + "project_list.json", 'utf8', function (err, data) {
             // console.log( data );
             // 发送 HTTP 头部
             // HTTP 状态值: 200 : OK
@@ -13,4 +13,19 @@ module.exports = function(app) {
             res.end( data );
         });
      })
+
+     app.get('/group/list', function (req, res) {
+        fs.readFile( path + "group_list.json", 'utf8', function (err, data) {
+            // console.log( data );
+            // 发送 HTTP 头部
+            // HTTP 状态值: 200 : OK
+            // 内容类型: application/json
+            res.writeHead(200, {'Content-Type': 'application/json;charset=UTF-8',
+                                'Accept': '*/*'});
+            res.end( data );
+        });
+     })
+
+
+
 }
